6726408687b1cc878395c6d88e4bb16cf4dddd5c
Udoy Rahman <37733863+Udoy2@users.noreply.github.com>
2024-12-25 22:21:50 +0600

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,37 +1,36 @@
-import React from 'react';
-import Tilt from 'react-tilt';
+import React from 'react'
+import Tilt from 'react-tilt'
 import { motion } from 'framer-motion';
 
 import { styles } from '../styles';
 import { services } from '../constants';
-import { fadeIn, textVariant } from '../utils/motion';
+import { fadeIn, textVariant } from '../utils/motion'
 import { SectionWrapper } from '../hoc';
-
 const ServiceCard = ({ index, title, icon }) => {
   return (
-    <Tilt className="xs:w-[250px] w-full">
+    <Tilt
+      className="xs:w-[250px] w-full"
+
+    >
       <motion.div
         variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
-        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+        className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
       >
         <div
           options={{
             max: 45,
             scale: 1,
-            speed: 450,
+            speed: 450
           }}
-          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+          className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
         >
           <img src={icon} alt={title} className="w-16 h-16 object-contain" />
-          <h3 className="text-white text-[20px] font-bold text-center">
-            {title}
-          </h3>
+          <h3 className='text-white text-[20px] font-bold text-center'>{title}</h3>
         </div>
       </motion.div>
     </Tilt>
-  );
-};
-
+  )
+}
 const About = () => {
   return (
     <>
@@ -39,33 +38,20 @@ const About = () => {
         <p className={`${styles.sectionSubText}`}>Introduction</p>
         <h2 className={`${styles.sectionHeadText}`}>Overview.</h2>
       </motion.div>
-
-      <div className="mt-4 flex flex-col md:flex-row items-center gap-10">
-        {/* Text Section */}
-        <motion.p
-          variants={fadeIn("", "", 0.1, 1)}
-          className="text-secondary text-[17px] max-w-3xl leading-[30px]"
-        >
-          I'm a skilled software developer, and I have over two years of
-          experience in web development, with a strong focus on the MERN stack
-          (MongoDB, Express.js, React.js, and Node.js). I am passionate about
-          building scalable web applications and have worked on various
-          projects, including dynamic e-commerce websites. In addition to my
-          work with the MERN stack, I have experience with automation tools such
-          as Puppeteer, Selenium, and Playwright. I am always eager to learn and
-          explore new tools and technologies, including Docker and Kubernetes,
-          to further enhance my skills in web development and DevOps.
-        </motion.p>
-
-
-
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        I'm a skilled software developer, and I have over four years of experience in web development, with a strong focus on the MERN stack (MongoDB, Express.js, React.js, and Node.js). I am passionate about building scalable web applications and have worked on various projects, including dynamic e-commerce websites. In addition to my work with the MERN stack, I have experience with automation tools such as Puppeteer, Selenium, and Playwright. I am always eager to learn and explore new tools and technologies, including Docker and Kubernetes, to further enhance my skills in web development and DevOps.
+      </motion.p>
       <div className="mt-20 flex flex-wrap gap-10">
-        {services.map((service, index) => (
-          <ServiceCard key={service.title} index={index} {...service} />
-        ))}
+        {services.map((service, index) => {
+
+          return <ServiceCard key={service.title} index={index} {...service} />
+        })}
       </div>
     </>
-  );
-};
+  )
+}
 
-export default SectionWrapper(About, "about");
+export default SectionWrapper(About,"about")
