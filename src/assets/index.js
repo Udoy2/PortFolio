d6475f4c8121e83de1985abbf91fe15e6b7e2ea8
udoy <udoyrahman983@gmail.com>
2025-08-05 11:46:59 +0600

    Fix import path for qualfixFab asset to use .jpg extension

--- src/assets/index.js
+++ src/assets/index.js
@@ -38,7 +38,7 @@ import furniflex from "./furniflex.png";
 import githubExplorer from "./githubExplorer.png";
 import electronicGoddies from "./electronicGoddies.png";
 import imagetextify from "./imagetextify.png";
-import qualfixFab from "./qualfixFav.png";
+import qualfixFab from "./qualfixFav.jpg";
 import instantBoard from "./instantBoard.png";
 
 export {
